06584743db08143cde5df8bf519fd4354132b0b0
fahadbgnr <aminulislamfahad1@gmail.com>
2025-05-31 02:58:06 +0600

    jwt implement

--- src/context/AuthContext/AuthProvider.jsx
+++ src/context/AuthContext/AuthProvider.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
 import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut,  } from 'firebase/auth';
 import { auth } from '../../firebase/firebase.init';
+import axios from 'axios';
 
 
 
@@ -36,6 +37,22 @@ const AuthProvider = ({children}) => {
         const unSubscribe = onAuthStateChanged(auth, currentUser=>{
             setUser(currentUser);
             setLoading(false);
+            
+            if(currentUser?.email){
+                const userData = {email: currentUser.email};
+                
+                axios.post('http://localhost:3000/jwt', userData,{
+                    withCredentials: true
+                })
+                .then(res => {
+                    console.log('token after jwt',res.data)
+                    // const token = res.data.token;
+                    // localStorage.setItem("token", token)
+                })
+                .catch(error=>{
+                    console.log(error)
+                })
+            }
             console.log("user in the auth state change", currentUser)
         })
         return ()=>{
